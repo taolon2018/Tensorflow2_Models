a7548641923ec85a4534b0abd8d555f62c7ecf54
Tao Long <taolong.cmu@gmail.com>
2020-08-02 22:33:14 -0700

    fix

--- semantic_segmentation/FCN/train.py
+++ semantic_segmentation/FCN/train.py
@@ -19,7 +19,7 @@ def train(args):
 
     model = FCN8()
 
-    if not os.path.exists("./model_weight"):
+    if os.path.exists("./model_weight/model.h5"):
         model.load_weights("./model_weight/model.h5")
 
     loss_func = tf.keras.losses.SparseCategoricalCrossentropy()
